dce596b07d63284a48913408be41077bd8b6be2a
Cheasy101 <bulat.nureeff@yandex.ru>
2023-03-20 23:27:36 +0300

    не идеальная, но вроде все делающая версия

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -22,11 +22,38 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter action (1 - get a file, 2 - save a file, 3 - delete a file): > ");
             int action = scanner.nextInt();
+
             dataOutputStream.writeInt(action); //отправили значение серверу
 
             switch (action) {
                 case (1):
-                    // TODO: 19.03.2023  make an action
+                    Scanner sc = new Scanner(System.in);
+                    System.out.println("Do you want to get the file by name or by id (1 - name, 2 - id):");
+
+                    String recieveAction = sc.nextLine();
+                    if (recieveAction.equals("1")) {
+
+                        System.out.println("Enter name of file");
+                        Scanner sc1 = new Scanner(System.in);
+                        String a = sc1.nextLine();
+                        dataOutputStream.writeUTF(a);
+
+                        writeBytesInNewFile(a, getArrayOfBytes()); //почему-то если все оставлять в 1 методе, то выполняется очень долго
+                        System.out.println(dataInputStream.readUTF());
+                        // получаем по имени
+
+                    } else if (recieveAction.equals("2")) {
+                        // получаем по id
+
+                        System.out.println("Enter id of file");
+                        Scanner sc1 = new Scanner(System.in);
+                        String a = sc1.nextLine();
+                        dataOutputStream.writeUTF(a);
+
+                        writeBytesInNewFile(dataInputStream.readUTF(), getArrayOfBytes());
+                        System.out.println(dataInputStream.readUTF());
+                    }
+
                     break;
                 case (2):
                     // TODO: 19.03.2023  make an action
@@ -35,34 +62,47 @@ public class Main {
                     System.out.println("Enter name of the file to be saved on server: > ");
                     String nameOfNewFIle = scanner.next();
 
-                    if (nameOfNewFIle.equals("")) {
-                        nameOfNewFIle = nameOfCurrentClientFile;
-                    }
-
                     dataOutputStream.writeUTF(nameOfNewFIle);
                     System.out.println("Отправили имя ");
 
-                    // TODO: 20.03.2023 обработать исключения. я неправильно сделал 
-                    
-//                    if (dataInputStream.readUTF().equals("Status Code 403")) {
-//                        System.out.println("Ошибка, такое имя уже есть в базе");
-//                    } else {
-                        sendFile(nameOfCurrentClientFile);
+
+                    // TODO: 20.03.2023 обработать исключения. я неправильно сделал
+
+                    if (dataInputStream.readUTF().equals("Status Code 403")) {
+                        System.out.println("Ошибка, такое имя уже есть в базе");
+                    } else {
+                        sendFile(nameOfCurrentClientFile, pathToClientData);
+
                         System.out.println("The request was sent.");
-//                    }
 
+                        int getId = dataInputStream.readInt();
+                        System.out.println("Response says that file is saved! ID > " + getId);
 
-//                    dataInputStream.close();
-//                    dataOutputStream.close();
-
-                    // должен считать имя файла и по байтам передать его на сервер
-                    /**
-                     * Сейчас я должен как-то грамотно ообработать на сервере все это
-                     */
-                    break;
-                case (3):
+                        break;
+                    }
+                case (3): {
                     // TODO: 19.03.2023  make an action
-                    break;
+                    System.out.println("Do you want to delete the file by name or by id (1 - name, 2 - id): > ");
+
+                    String deleteAction = scanner.next();
+                    switch (deleteAction) {
+                        case "1" -> {
+                            System.out.println("Enter name of file");
+                            Scanner sc1 = new Scanner(System.in);
+                            String a = sc1.nextLine();
+                            dataOutputStream.writeUTF(a);
+                            System.out.println("отправили имя " + a);
+                            System.out.println(dataInputStream.readUTF());
+                        }
+                        case "2" -> { //почемуто когда работаешь через 1 сканнер все ломается
+                            System.out.println("Enter id of file");
+                            Scanner sc2 = new Scanner(System.in);
+                            String b = sc2.nextLine();
+                            dataOutputStream.writeUTF(b);
+                            System.out.println(dataInputStream.readUTF());
+                        }
+                    }
+                }
             }
 
             // TODO: 19.03.2023  прописать логику для того, как сервер команты обрабатывает.
@@ -73,23 +113,105 @@ public class Main {
         }
     }
 
-    public static void sendFile(String fileName) throws IOException {
+//    private static void doFirstAction(int nextStep) throws IOException { //1 - name, 2 - id
+//        Scanner scanner = new Scanner(System.in);
+//        switch (nextStep) {
+//            case 1:
+//                Scanner sc = new Scanner(System.in);
+//                System.out.println("Do you want to get the file by name or by id (1 - name, 2 - id):");
+//                String recieveAction = sc.nextLine();
+//                if (recieveAction.equals("1")) {
+//                    System.out.println("Enter name of file");
+//                    Scanner sc1 = new Scanner(System.in);
+//                    String a = sc1.nextLine();
+//                    dataOutputStream.writeUTF(a);
+//                    writeBytesInNewFile(a, getArrayOfBytesFromSocketStream());
+//                    System.out.println(dataInputStream.readUTF());
+//                    // получаем по имени
+//                } else if (recieveAction.equals("2")) {
+//                    // получаем по id
+//                    System.out.println("Enter id of file");
+//                    Scanner sc1 = new Scanner(System.in);
+//                    String a = sc1.nextLine();
+//                    dataOutputStream.writeUTF(a);
+//                    //  String name = "C:\\Users\\fikus\\IdeaProjects\\se-lab04-tmp2223\\src\\main\\java\\client\\data\\" + a;
+//
+//                    writeBytesInNewFile(dataInputStream.readUTF(), getArrayOfBytesFromSocketStream());
+//                    System.out.println(dataInputStream.readUTF());
+//                }
+//
+//            case 2:
+//                System.out.println("Enter id: > ");
+//                // тут сейчас принимать файл будем
+//                int fileId = scanner.nextInt();
+////                receiveData(nameOfFile);
+//
+//        }
+//    }
+
+//    public static void receiveData(String nameOfFile) throws IOException {
+//        int bytes = 0;
+//        File file = new File(pathToClientData, nameOfFile); //сложили место и имя файла
+//        FileOutputStream fileOutputStream = new FileOutputStream(file);
+//
+//        long size = dataInputStream.readLong();
+//        byte[] buffer = new byte[4 * 1024];
+//        while (size > 0
+//                && (bytes = dataInputStream.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
+//            fileOutputStream.write(buffer, 0, bytes);
+//            size -= bytes;
+//            System.out.println("переносим переносим");
+//        }
+//        System.out.println("File is Received");
+//    }
+
+    private static byte[] getArrayOfBytes() throws IOException {
+        int length = (int) dataInputStream.readLong();
+        byte[] resultBytes = new byte[length];
+        dataInputStream.readFully(resultBytes, 0, length);
+        return resultBytes; //получаю как бы файл из текущего потока
+    }
+
+    // по заданному имени создает файл в data и байты из массива записывает в новый файл
+    private static void writeBytesInNewFile(String nameOfFile, byte[] bytes) throws FileNotFoundException {
+
+        File file = new File(pathToClientData, nameOfFile);
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        try {
+            fileOutputStream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendFile(String fileName, String pathToClientData) throws IOException {
 
         int bytes = 0;
 
         File file = new File(pathToClientData, fileName);
-        FileInputStream fileInput = new FileInputStream(file);
+        FileInputStream fileInputStream = new FileInputStream(file);
 
-        dataOutputStream.writeLong(file.length()); // отправляем байтовую длинну файла
 
-        byte[] buffer = new byte[4 * 1024];
-        while ((bytes = fileInput.read(buffer)) != -1) {
-            dataOutputStream.write(buffer, 0, bytes);
-            dataOutputStream.flush();
+        dataOutputStream.writeLong(file.length());
+        // тут массив может быть очень большой, необходимо будет это учитывать потом
+        byte[] buffer = new byte[(int) file.length()];
+        // в переменнную буфер считываем все байты из файла
+        fileInputStream.read(buffer);
+        for (int i = 0; i < buffer.length; i++) {
+            System.out.println(buffer[i]);
         }
-        dataInputStream.close();
-    }
+        // отправили через сокет
+        dataOutputStream.write(buffer);
 
+//        dataOutputStream.writeLong(file.length()); // отправляем байтовую длинну файла
+//
+//        byte[] buffer = new byte[(int) file.length()];
+//
+//        while ((bytes = fileInput.read(buffer)) != -1) {
+//            dataOutputStream.write(buffer, 0, bytes);
+//            dataOutputStream.flush();
+//        }
+    }
 }
 
 
--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -4,10 +4,8 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class Main {
     private static final int PORT = 34522;
@@ -15,7 +13,7 @@ public class Main {
 
     static String pathToServerTextDatabase = "C:\\Users\\Bulat\\IdeaProjects\\SaveAnything\\se-lab04-MySolution\\src\\main\\java\\server\\database.txt";
 
-    static HashMap<String, Integer> serverDataBase = new HashMap<>();
+    static HashMap<String, String> serverDataBase = new HashMap<>();
 
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(PORT)) {
@@ -47,42 +45,83 @@ class Session extends Thread {
             dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
             int actionVar = dataInputStream.readInt();
-//            String nameOfUser = dataInputStream.readUTF(); //читаем че юзер говорит
             System.out.println("Получили action: " + actionVar);
-//            actions(actionVar);
 
             switch (actionVar) {
                 case (1):
                     // TODO: 19.03.2023  make an action
+
+                    updateHasMup();
+                    String idOrName = dataInputStream.readUTF();
+                    if (isNumber(idOrName)) { //если число
+
+                        if (Main.serverDataBase.containsValue(idOrName)) {
+                            System.out.println("поняли, что по айди работаем");
+                            File file = new File(Main.pathToServerData, getKey(Main.serverDataBase, idOrName));
+                            dataOutputStream.writeUTF(getKey(Main.serverDataBase, idOrName));
+                            sendFileServerVersion(file.getPath());
+                        }
+                    } else {
+                        File file = new File(Main.pathToServerData, idOrName);
+                        sendFileServerVersion(file.getPath());
+                    }
                     break;
+
+
                 case (2):
                     // TODO: 19.03.2023  make an action
 
-                    updateHasMup(); //обновляем базу
-
-                    for (Map.Entry<String, Integer> entry : Main.serverDataBase.entrySet()) {
-                        System.out.println(entry.getKey() + " " + entry.getValue() + "\n");
-                    }
-
                     String nameOfFile = dataInputStream.readUTF();
+                    System.out.println("имя файла " + nameOfFile);
+
+                    updateHasMup(); //обновляем базу
 
                     /**
                      * Типа если такое имя уже есть в базе, то ошибка
                      */
 
-//                    if (Main.serverDataBase.containsKey(nameOfFile)) {
-//                        System.out.println("Status code 403");
-//                        dataOutputStream.writeUTF("Status Code 403");
-//                        break;
-//                    } else { todo я неправильно обрабатываю исключения
+                    if (Main.serverDataBase.containsKey(nameOfFile)) {
+                        System.out.println("Status code 403");
+                        dataOutputStream.writeUTF("Status Code 403");
+                        break;
+                    } else {
 
-                    System.out.println("Получили имя " + nameOfFile);
-                    acceptAndSaveFile(nameOfFile);
-//                    }
+                        System.out.println("Получили имя " + nameOfFile);
+                        acceptAndSaveFile(nameOfFile); //сохранили файл
+
+                    }
                     break;
-                case (3):
+
+                case (3): {
                     // TODO: 19.03.2023  make an action
-                    break;
+                    System.out.println("попали в состояние удалить");
+                    updateHasMup();
+                    String nameOrId = dataInputStream.readUTF();
+                    System.out.println("имя файла " + nameOrId);
+
+                    if (isNumber(nameOrId)) { //если число
+
+                        if (Main.serverDataBase.containsValue(nameOrId)) {
+                            System.out.println("поняли, что по айди работаем");
+                            File file = new File(Main.pathToServerData, getKey(Main.serverDataBase, nameOrId));
+                            if (file.delete()) { // если успешно удалили, то перезаписываем базу
+                                updateHasMup();
+                                System.out.println("ну вот хэшмап должен был обновиться");
+                                Main.serverDataBase = rewriteHashMup(nameOrId);
+                                dataOutputStream.writeUTF("The response says that this file with id > " + nameOrId + " deleted successfully!");
+                            } else System.out.println("The response says that this file is not found!");
+                        }
+                    } else if (!isNumber(nameOrId)) {
+                        System.out.println("поняли, что по имени работаем");
+                        File file = new File(Main.pathToServerData, nameOrId);
+                        if (file.delete()) { // если успешно удалили, то перезаписываем базу
+                            updateHasMup();
+                            Main.serverDataBase = rewriteHashMup(nameOrId);
+                            dataOutputStream.writeUTF("The response says that this file with id > " + nameOrId + " deleted successfully!");
+                        } else System.out.println("The response says that this file is not found!");
+                    }
+                }
+                break;
             }
 
 //            output.writeUTF(nameOfUser);
@@ -105,9 +144,9 @@ class Session extends Thread {
 
 
     public static void acceptAndSaveFile(String fileName) throws IOException {
-
-        int bytes = 0;
         Random random = new Random();
+        int bytes = 0;
+
         File file = new File(Main.pathToServerData, fileName); //сложили место и имя файла
         System.out.println("сложили место и имя файла");
         FileOutputStream fileOutputStream = new FileOutputStream(file);
@@ -122,7 +161,7 @@ class Session extends Thread {
             } else flag = false;
         }
 
-        Main.serverDataBase.put(fileName, uniqId); // добавили значение в хешмап
+        Main.serverDataBase.put(fileName, String.valueOf(uniqId)); // добавили значение в хешмап
         System.out.println("добавили значение в хешмап");
         addFileToTextServerDatabase(); // добавили эти значения в файл
 
@@ -143,6 +182,9 @@ class Session extends Thread {
         }
 
         System.out.println("File is Received");
+
+        dataOutputStream.writeInt(uniqId); // отправляем айдишник пользователю
+        System.out.println("status code 200");
 //        fileOutputStream.close();
     }
 
@@ -156,7 +198,7 @@ class Session extends Thread {
             while (sc.hasNext()) {
                 String currentLine = sc.nextLine();
                 String[] splitCurrentLine = currentLine.split(" ");
-                Main.serverDataBase.put(splitCurrentLine[0], Integer.valueOf(splitCurrentLine[1]));
+                Main.serverDataBase.put(splitCurrentLine[0], splitCurrentLine[1]);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -166,7 +208,7 @@ class Session extends Thread {
 
     public static void addFileToTextServerDatabase() throws IOException {
         try (FileWriter pathToServerTextDatabase = new FileWriter(Main.pathToServerTextDatabase);) {
-            for (Map.Entry<String, Integer> entry : Main.serverDataBase.entrySet()) {
+            for (Map.Entry<String, String> entry : Main.serverDataBase.entrySet()) {
                 pathToServerTextDatabase.write(entry.getKey() + " " + entry.getValue() + "\n"); //записали имя файла в текст
             }
             updateHasMup();
@@ -175,4 +217,79 @@ class Session extends Thread {
         }
     }
 
+//    public static void sendFileServerVersion(String fileName, String pathToClientData) throws IOException {
+//
+//        int bytes = 0;
+//
+//        File file = new File(pathToClientData, fileName);
+//        FileInputStream fileInput = new FileInputStream(file);
+//
+//
+//        dataOutputStream.writeLong(file.length());
+//        // тут массив может быть очень большой, необходимо будет это учитывать потом
+//        byte[] buffer = new byte[(int) file.length()];
+//        // в переменнную буфер считываем все байты из файла
+//        fileInput.read(buffer);
+//        for (int i = 0; i < buffer.length; i++) {
+//            System.out.println(buffer[i]);
+//        }
+//        // отправили через сокет
+//        dataOutputStream.write(buffer);
+//
+////        dataOutputStream.writeLong(file.length()); // отправляем байтовую длинну файла
+////
+////        byte[] buffer = new byte[(int) file.length()];
+////
+////        while ((bytes = fileInput.read(buffer)) != -1) {
+////            dataOutputStream.write(buffer, 0, bytes);
+////            dataOutputStream.flush();
+////        }
+//    }
+
+    public static void sendFileServerVersion(String fileName) throws IOException {
+
+        File file = new File(fileName);
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        dataOutputStream.writeLong(file.length());
+        byte[] buffer = new byte[(int) file.length()];
+        fileInputStream.read(buffer);
+
+        for (int i = 0; i < buffer.length; i++) {
+            System.out.println(buffer[i]);// просто проверка, что отправляется все
+        }
+        dataOutputStream.write(buffer);
+
+    }
+
+    public static boolean isNumber(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static <K, V> K getKey(Map<K, V> map, V value) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            if (value.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public static HashMap<String, String> rewriteHashMup(String nameOrId) {
+        HashMap<String, String> map = new HashMap<>();
+        List<String> keys = new ArrayList<String>(Main.serverDataBase.keySet());
+        for (int i = 0; i < keys.size(); i++) {
+            String key = keys.get(i);
+            String value = Main.serverDataBase.get(key);
+            if (!(key.equals(nameOrId) || value.equals(nameOrId))) {
+                map.put(key, value);
+            }
+        }
+        return map;
+    }
 }
