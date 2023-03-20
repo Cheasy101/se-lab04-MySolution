c92d9193ebdc79e904b0c19d860a4dfad9dc0656
Cheasy101 <bulat.nureeff@yandex.ru>
2023-03-20 19:29:51 +0300

    не идеальная, но рабочая версия добавления файла в базу сервера

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -1,7 +1,97 @@
 package client;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // todo - your java-code
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 34522;
+    private static DataOutputStream dataOutputStream;
+    private static DataInputStream dataInputStream;
+
+    static String pathToClientData = "C://Users//Bulat//IdeaProjects//SaveAnything//se-lab04-MySolution//src//main//java//client//data";
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+            Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataInputStream = new DataInputStream(socket.getInputStream());
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter action (1 - get a file, 2 - save a file, 3 - delete a file): > ");
+            int action = scanner.nextInt();
+            dataOutputStream.writeInt(action); //отправили значение серверу
+
+            switch (action) {
+                case (1):
+                    // TODO: 19.03.2023  make an action
+                    break;
+                case (2):
+                    // TODO: 19.03.2023  make an action
+                    System.out.println("Enter name of the file: > ");
+                    String nameOfCurrentClientFile = scanner.next();
+                    System.out.println("Enter name of the file to be saved on server: > ");
+                    String nameOfNewFIle = scanner.next();
+
+                    if (nameOfNewFIle.equals("")) {
+                        nameOfNewFIle = nameOfCurrentClientFile;
+                    }
+
+                    dataOutputStream.writeUTF(nameOfNewFIle);
+                    System.out.println("Отправили имя ");
+
+                    // TODO: 20.03.2023 обработать исключения. я неправильно сделал 
+                    
+//                    if (dataInputStream.readUTF().equals("Status Code 403")) {
+//                        System.out.println("Ошибка, такое имя уже есть в базе");
+//                    } else {
+                        sendFile(nameOfCurrentClientFile);
+                        System.out.println("The request was sent.");
+//                    }
+
+
+//                    dataInputStream.close();
+//                    dataOutputStream.close();
+
+                    // должен считать имя файла и по байтам передать его на сервер
+                    /**
+                     * Сейчас я должен как-то грамотно ообработать на сервере все это
+                     */
+                    break;
+                case (3):
+                    // TODO: 19.03.2023  make an action
+                    break;
+            }
+
+            // TODO: 19.03.2023  прописать логику для того, как сервер команты обрабатывает.
+
+        } catch (
+                IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public static void sendFile(String fileName) throws IOException {
+
+        int bytes = 0;
+
+        File file = new File(pathToClientData, fileName);
+        FileInputStream fileInput = new FileInputStream(file);
+
+        dataOutputStream.writeLong(file.length()); // отправляем байтовую длинну файла
+
+        byte[] buffer = new byte[4 * 1024];
+        while ((bytes = fileInput.read(buffer)) != -1) {
+            dataOutputStream.write(buffer, 0, bytes);
+            dataOutputStream.flush();
+        }
+        dataInputStream.close();
+    }
+
 }
+
+
+
+
--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,7 +1,178 @@
 package server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    private static final int PORT = 34522;
+    static String pathToServerData = "C://Users//Bulat//IdeaProjects//SaveAnything//se-lab04-MySolution//src//main//java//server//data";
+
+    static String pathToServerTextDatabase = "C:\\Users\\Bulat\\IdeaProjects\\SaveAnything\\se-lab04-MySolution\\src\\main\\java\\server\\database.txt";
+
+    static HashMap<String, Integer> serverDataBase = new HashMap<>();
+
     public static void main(String[] args) {
-        // todo - your java-code
+        try (ServerSocket server = new ServerSocket(PORT)) {
+
+            System.out.println("Сервер запущен и готов к работе...");
+            while (true) {
+                Session session = new Session(server.accept());
+                session.start();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+}
+
+class Session extends Thread {
+    private final Socket socket;
+    private static DataOutputStream dataOutputStream;
+    private static DataInputStream dataInputStream;
+
+    public Session(Socket socketForClient) {
+        this.socket = socketForClient;
+    }
+
+    public void run() {
+        try {
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            int actionVar = dataInputStream.readInt();
+//            String nameOfUser = dataInputStream.readUTF(); //читаем че юзер говорит
+            System.out.println("Получили action: " + actionVar);
+//            actions(actionVar);
+
+            switch (actionVar) {
+                case (1):
+                    // TODO: 19.03.2023  make an action
+                    break;
+                case (2):
+                    // TODO: 19.03.2023  make an action
+
+                    updateHasMup(); //обновляем базу
+
+                    for (Map.Entry<String, Integer> entry : Main.serverDataBase.entrySet()) {
+                        System.out.println(entry.getKey() + " " + entry.getValue() + "\n");
+                    }
+
+                    String nameOfFile = dataInputStream.readUTF();
+
+                    /**
+                     * Типа если такое имя уже есть в базе, то ошибка
+                     */
+
+//                    if (Main.serverDataBase.containsKey(nameOfFile)) {
+//                        System.out.println("Status code 403");
+//                        dataOutputStream.writeUTF("Status Code 403");
+//                        break;
+//                    } else { todo я неправильно обрабатываю исключения
+
+                    System.out.println("Получили имя " + nameOfFile);
+                    acceptAndSaveFile(nameOfFile);
+//                    }
+                    break;
+                case (3):
+                    // TODO: 19.03.2023  make an action
+                    break;
+            }
+
+//            output.writeUTF(nameOfUser);
+//            System.out.println("Отправили обратно ");
+//
+//            double a = dataInputStream.readDouble();
+//            System.out.println("получили первое число");
+//            double b = dataInputStream.readDouble();
+//            System.out.println("Получили второе число");
+//
+//            Double afterMath = (3 * Math.pow(Math.cos(a - (Math.PI / 6)), 2)) / (1 / 2 + Math.sin(b * b));
+//            output.writeDouble(afterMath);
+////            output.writeUTF(String.valueOf(afterMath));
+//
+//            System.out.println("Отправили значение ");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void acceptAndSaveFile(String fileName) throws IOException {
+
+        int bytes = 0;
+        Random random = new Random();
+        File file = new File(Main.pathToServerData, fileName); //сложили место и имя файла
+        System.out.println("сложили место и имя файла");
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+
+        int uniqId = random.nextInt(0, 100);
+        boolean flag = true;
+
+        while (flag) {
+            if (Main.serverDataBase.containsValue(uniqId)) {
+                uniqId = random.nextInt(0, 100);
+            } else flag = false;
+        }
+
+        Main.serverDataBase.put(fileName, uniqId); // добавили значение в хешмап
+        System.out.println("добавили значение в хешмап");
+        addFileToTextServerDatabase(); // добавили эти значения в файл
+
+
+        /**
+         * т.е грубо говоря пока я не создам уникальный айди - я продолжаю его генерить.
+         */
+
+        System.out.println("Дошли до переноса файла");
+
+        long size = dataInputStream.readLong();
+        byte[] buffer = new byte[4 * 1024];
+        while (size > 0
+                && (bytes = dataInputStream.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
+            fileOutputStream.write(buffer, 0, bytes);
+            size -= bytes;
+            System.out.println("переносим переносим");
+        }
+
+        System.out.println("File is Received");
+//        fileOutputStream.close();
+    }
+
+    static File file = new File(Main.pathToServerTextDatabase);
+
+    public static void updateHasMup() { // получаем хешмап со всеми значениями, что есть в файле
+
+        try (Scanner sc = new Scanner(file, StandardCharsets.UTF_8)) {
+// тут проблема в том, что мне надо грамотно распарсить файл, но сейчас я попоробую хешмапом сделать
+
+            while (sc.hasNext()) {
+                String currentLine = sc.nextLine();
+                String[] splitCurrentLine = currentLine.split(" ");
+                Main.serverDataBase.put(splitCurrentLine[0], Integer.valueOf(splitCurrentLine[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void addFileToTextServerDatabase() throws IOException {
+        try (FileWriter pathToServerTextDatabase = new FileWriter(Main.pathToServerTextDatabase);) {
+            for (Map.Entry<String, Integer> entry : Main.serverDataBase.entrySet()) {
+                pathToServerTextDatabase.write(entry.getKey() + " " + entry.getValue() + "\n"); //записали имя файла в текст
+            }
+            updateHasMup();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
